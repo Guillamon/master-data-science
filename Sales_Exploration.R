b93c170ed83852b8b3379b4ab6e6517a21a7c7a8
Guillamon <joseguillamon.r@gmail.com>
2019-05-01 14:46:04 +0200

    Cleaned the code: had to change date formats, made period easy to change

--- Sales_Exploration.R
+++ Sales_Exploration.R
@@ -12,150 +12,92 @@ library(stringi)
 library (ggplot2)
 library(reshape2)
 
-# cleaning weird characters from the dataframe
-col_index <- grep("Global.Dimension.2.Code", colnames(query_ile))
-query_ile[,col_index]<-query_ile[,col_index] %>% lapply(stri_trans_general,"Latin-ASCII")
+customer_key_df <- as.data.frame(data.table::fread("Customer_Key.csv", sep=";")) %>% 
+  mutate(Original_Code = as.character(Original_Code)) %>%  #changing Original_Code from numeric to character
+  select(-Campaign_Name)
 
-# joining both tables, and filtering the result
+#setting period variable
+period <- "month"
+
+# cleaning weird characters from the dataframe (BETTER FIND OTHER METHOD)
+#col_index <- grep("Global.Dimension.2.Code", colnames(query_ile))
+#query_ile[,col_index]<-query_ile[,col_index] %>% lapply(stri_trans_general,"Latin-ASCII")
+
+# first joining both item ledger and sales shipment tables, and filtering the result
+# second, right join with customer key, to get only customers that interest us
 joined_sales <- query_ile %>% 
   left_join(query_ssh, by= c("Document No_" = "No_"),suffix = c("_ile", "_ssh"))%>% 
   filter(Correction_ile == 0) %>%   #not interested in corrections for the moment, may affect date
   filter (Quantity <0) %>%    #not interested in positive sales entries (returns, corrections)
   filter(`Gen_ Bus_ Posting Group` == "TUZEMSKO")%>%    #leaving out sales to other countries, only want inland
-  filter (`Customer Price Group` != 'GASTRO')   #leaving out horeca clients for the moment too
+  filter (`Customer Price Group` != 'GASTRO') %>%   #leaving out horeca clients for the moment too
+  right_join(customer_key_df, by = c("Bill-to Customer No_" = "Original_Code")) %>%
+  select (Customer_Code,`Bill-to Name`, everything()) %>% 
+  right_join(query_weights, by =c("Item No_" = "No_")) %>% #joining with df that contains volume for each item
+  mutate(Quantity = Quantity *-1) %>% #database registers units shipped as negative
+  mutate(Quantity = Quantity * `Net Weight`) #getting sales in litres
+
+  
 
 #checking that we are left only with customers that interest us
 joined_sales %>% group_by(`Customer Price Group`) %>%  tally()
 
 
+
 #-------------------------------------------------------------------------------------------------------------------
 #En principio nos vamos a quedar con el order date, mejor que shipment date
 
-#preparing a table that will have weekly sales of items as rows, and every item as a column
+#preparing a table that will have monthly / weekly sales of items as rows, and every item as a column
 sales_df_item <- joined_sales %>%
-  filter(`Global Dimension 2 Code` != "CALVO") %>%
-  mutate(init_week_day = floor_date(`Order Date`,"week")) %>% 
-  group_by(init_week_day, `Item No_`) %>% 
-  summarise(Sum_Quantity = sum(Quantity)) %>% 
+  mutate(init_period_date = floor_date(`Shipment Date`,period) %>% as_date()) %>% 
+  group_by(init_period_date, `Item No_`) %>% 
+  summarise(Sum_Quantity = sum(Quantity)) %>%
   ungroup()
 
-#getting positive values
-sales_df_item$Sum_Quantity <- sales_df_item$Sum_Quantity*(-1)
 
-#spreading initial table to get final intended result
-sales_df_item <- sales_df_item %>% spread(`Item No_`,Sum_Quantity)
-
-#filling in NAs with zeros
-sales_df_item[is.na(sales_df_item)] <- 0
-
-#adding a column with week no_, might be useful when sorting  
-sales_df_item <- sales_df_item%>% mutate (week_num = as.numeric(rownames(sales_df_item)))
+#creating a new dataframe with our best_sellers by sum - 
+#useful to get rid of promotional products
+#ALTERNATIVE: (creating a new dataframe with our best_sellers by mean -useful to show products that at some moment have had great sales)
+top_items <- sales_df_item %>% 
+  filter (year(init_period_date) > 2017) %>%
+  group_by(`Item No_`) %>% 
+  summarise(sum_sales = sum(Sum_Quantity)) %>%
+  ungroup() %>%
+  arrange(desc(sum_sales)) #%>% 
+  #filter(sum_sales > 13000)
 
 
 #--------------------------------------------------------------------------------------------------------------------
 
 #preparing a table that will have weekly sales of brands as rows, and every brand as a column
 sales_df_brand <- joined_sales %>%   
-  mutate(init_week_day = floor_date(`Order Date`,"week")) %>% 
-  group_by(init_week_day, `Global Dimension 2 Code`) %>% 
+  mutate(init_period_date = floor_date(`Order Date`,period) %>% as_date()) %>% 
+  group_by(init_period_date, `Global Dimension 2 Code`) %>% 
   summarise(Sum_Quantity = sum(Quantity)) %>% 
   ungroup()
 
 
-sales_df_brand$Sum_Quantity <- sales_df_brand$Sum_Quantity*(-1)
-sales_df_brand <- sales_df_brand %>% 
-  spread(`Global Dimension 2 Code`,Sum_Quantity) 
-
-#filling in NAs with zeros
-sales_df_brand[is.na(sales_df_brand)] <- 0
 #-----------------------------------------------------------------------------------------------------------------
 
-#preparing a df for a stacked ts chart for brands
-sales_df_brand_2 <- joined_sales %>%   
-  mutate(init_week_day = floor_date(`Order Date`,"week")) %>% 
-  group_by(init_week_day, `Global Dimension 2 Code`) %>% 
-  summarise(Sum_Quantity = sum(Quantity)) %>% 
-  ungroup()
+# charts for brand
+ggplot(data = sales_df_brand , aes(x=init_period_date,y=Sum_Quantity)) +
+  geom_line(aes(color= "dark orange"))
 
-sales_df_brand_2$Sum_Quantity <- sales_df_brand_2$Sum_Quantity*(-1)
-
-#making a df for a stacked ts chart for items
-sales_df_item_2 <- joined_sales %>%   
-  filter(`Global Dimension 2 Code` != "CALVO") %>%
-  mutate(init_week_day = floor_date(`Order Date`,"week")) %>% 
-  group_by(init_week_day, `Item No_`) %>% 
-  summarise(Sum_Quantity = sum(Quantity)) %>%  
-  ungroup()
-
-sales_df_item_2$Sum_Quantity <- sales_df_item_2$Sum_Quantity*(-1)
-
-
-#sales_df_item_2 %>% count(`Item No_`) %>% View()
-
-#creating a new dataframe with our best_sellers by mean - 
-#useful to show products that at some moment have had great sales
-#top_items <- sales_df_item_2 %>% 
-#filter (year(init_week_day) > 2017) %>% 
-#group_by(`Item No_`) %>% 
-#summarise(mean_sales = mean(Sum_Quantity)) %>% 
-#arrange(desc(mean_sales)) %>% 
-#ungroup() %>% 
-#head(5)
-
-#creating a new dataframe with our best_sellers by sum - 
-#useful to get rid of promotional products
-top_items <- sales_df_item_2 %>% 
-  filter (year(init_week_day) > 2017) %>%
-  filter(`Item No_`!='R241381') %>% 
-  filter(`Item No_`!='R240681') %>% 
-  group_by(`Item No_`) %>% 
-  summarise(sum_sales = sum(Sum_Quantity)) %>% 
-  arrange(desc(sum_sales)) %>% 
-  ungroup() %>% 
-  filter(sum_sales >5000)
-#%>% 
-#head(5)
-
-#innner join of our best sellers with timeseries df
-sales_df_top_items <- sales_df_item_2 %>% inner_join(top_items)
-
-
-#-----------------------------------------------------------------------------------------------------------------
-
-
-
-# charts for brands
-ggplot(data = sales_df_brand , aes(x=init_week_day,y=CALVO)) +
-  geom_line()
-ggplot(data = sales_df_brand , aes(x=init_week_day,y=LIMENA)) +
-  geom_line()
-
-# charts for items
-ggplot(data = sales_df_item , aes(x=init_week_day,y=A020120)) +
-  geom_line()
-ggplot(data = sales_df_item , aes(x=init_week_day,y=R242031)) +
-  geom_line()
-
-#stacked charts for brands
-ggplot(sales_df_brand_2, aes(x = init_week_day, y = Sum_Quantity)) + 
-  geom_line(aes(color=`Global Dimension 2 Code`)) + 
-  facet_wrap(~ `Global Dimension 2 Code`, scales = 'free_y', ncol = 1)
 
 #stacked charts for bestseller items
-ggplot(sales_df_top_items, aes(x = init_week_day, y = Sum_Quantity)) + 
+ggplot(sales_df_top_items, aes(x = init_period_date, y = Sum_Quantity)) + 
   geom_line(aes(color=`Item No_`)) + 
   facet_wrap(~ `Item No_`, scales = 'free_y', ncol = 1)
 
 
-#------------------------------------------------------------------------------------------------------------
+#EXPORT CHARTS TO PNG --------------------------------------------------------------------------------
 
 
 # 1. open the file
-png("brands.png", width = 2700, height = 1800)
+png("limenita_month.png", width = 2700, height = 1800)
 # 2. Create the plot
-ggplot(sales_df_brand_2, aes(x = init_week_day, y = Sum_Quantity)) + 
-  geom_line(aes(color=`Global Dimension 2 Code`),size=2) + 
-  facet_wrap(~ `Global Dimension 2 Code`, scales = 'free_y', ncol = 1)+
+ggplot(data = sales_df_brand , aes(x=init_period_date,y=Sum_Quantity)) +
+  geom_line(aes(color= "dark orange", size=1)) +
   theme(legend.position="none",
         axis.text=element_text(size=25),
         axis.title=element_text(size=25,face="bold"),
@@ -164,10 +106,15 @@ ggplot(sales_df_brand_2, aes(x = init_week_day, y = Sum_Quantity)) +
 dev.off()
 
 
+
+sales_df_top_items <- sales_df_item %>% 
+  inner_join(head(top_items[21:30,]))
+
+
 # 1. open the file
-png("top_limenita.png", width = 2700, height = 1800)
+png("21-30_limenita_month.png", width = 2700, height = 1800)
 # 2. Create the plot
-ggplot(sales_df_top_items, aes(x = init_week_day, y = Sum_Quantity)) + 
+ggplot(sales_df_top_items, aes(x = init_period_date, y = Sum_Quantity)) + 
   geom_line(aes(color=`Item No_`),size=2) + 
   facet_wrap(~ `Item No_`, scales = 'free_y', ncol = 1) +
   theme(legend.position="none",
@@ -176,3 +123,6 @@ ggplot(sales_df_top_items, aes(x = init_week_day, y = Sum_Quantity)) +
         strip.text.x = element_text(size = 30, colour = "black"))
 # 3. Close the file
 dev.off()
+
+
+
