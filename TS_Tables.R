7d4b8f4d1be1b640dfc9a1e8684121e15ec9acef
Guillamon <joseguillamon.r@gmail.com>
2019-05-01 15:00:12 +0200

    Renamed file

--- TS_Tables.R
+++
@@ -1,4 +0,0 @@
-# Join sales and sales price data
-# Create a separate dataframe for each item
-# Stack columns for features: -customer-price, -customer-promotion_days, etc.
-# FIND OUT IF THERE RETAIL CHAINS LOOK AT WHAT OTHER CHAINS ARE PROMOTING TO TAKE DECISIONS!!!!!!
